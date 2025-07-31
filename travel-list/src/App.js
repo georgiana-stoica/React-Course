cc54b5aa8621dfd68457db0c03f0388d3c5fd248
georgiana-stoica <sgeo2001@gmail.com>
2025-07-31 13:26:06 +0300

    adding the logic

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,12 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+    {id: 1, description: "Passports", quantity: 2, packed: true},
+    {id: 2, description: "Socks", quantity: 12, packed: true},
+    {id: 3, description: "Charger", quantity: 1, packed: false}
+];
 
-function App() {
+export default function App() {
     return (
-        <div>
-            Hello!
+        <div className="app">
+            <Logo/>
+            <Form/>
+            <PackingList/>
+            <Stats/>
         </div>
     );
 }
 
-export default App;
+function Logo() {
+    return <h1>🌴 Far Away 💼</h1>
+}
+
+function Form() {
+    function handleSubmit(e) {
+        e.preventDefault(); //  to prevent the default browser actions hence we can use it to prevent the default action in event callbacks (like browser refresh)
+        console.log(e);
+    }
+
+    return <div className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your 😍 trip?</h3>
+        <select>
+            {Array.from({length: 20}, (_, i) => i + 1).map((num) =>(<option value={num} key={num}>{num}</option>))}
+        </select>
+        <form>
+            <input type="text" placeholder="Item..."/>
+        </form>
+        <button>ADD</button>
+    </div>
+}
+
+function PackingList() {
+
+    return (<div className="list">
+        <ul>
+            {initialItems.map(item => <Item item={item} key={item.id}/>)}
+        </ul>
+    </div>);
+}
+
+function Item({item}) {
+    return (
+        <div>
+            <li>
+                <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+                    {item.quantity} {item.description}
+                </span>
+                <button>❌</button>
+            </li>
+        </div>
+    );
+}
+
+function Stats() {
+    return (<>
+        <footer className="stats">
+            <em>
+                💼 You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    </>);
+}
