dea5008f6e62290540f48935f3352bd02ef2c675
georgiana-stoica <92844869+georgiana-stoica@users.noreply.github.com>
2025-10-18 18:27:36 +0300

    Update App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-
+//comment
 const initialItems = [
     {id: 1, description: "Passports", quantity: 2, packed: true},
     {id: 2, description: "Socks", quantity: 12, packed: true},
