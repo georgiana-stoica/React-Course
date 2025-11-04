6f84963dc8c4566bc8b4f1d06a8bd05dd33924d8
georgiana-stoica <sgeo2001@gmail.com>
2025-11-04 14:06:01 +0200

    last touch ups

--- accordion-component/src/App.js
+++ accordion-component/src/App.js
@@ -1,7 +1,69 @@
+import './styles.css'
+import {useState} from "react";
+
+const faqs = [
+    {
+        title: "Where are these chairs assembled?",
+        text:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    },
+    {
+        title: "How long do I have to return my chair?",
+        text:
+            "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    },
+    {
+        title: "Do you ship to countries outside the EU?",
+        text:
+            "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+    }
+];
+
 export default function App() {
-  return (
-    <div>
-      TODO
+    return (
+        <div>
+            <Accordion data={faqs}/>
+        </div>
+    );
+}
+
+function Accordion({data}) {
+    const [currOpen, setCurrOpen] = useState(null);
+
+    return (<div className="accordion">
+        {data.map((el, id) =>
+            (<AccordionItem
+                    currOpen={currOpen}
+                    onOpen={setCurrOpen}
+                    title={el.title}
+                    num={id}
+                    key={el.title}>
+                    {el.text}
+                </AccordionItem>
+            ))}
+        <AccordionItem
+            currOpen={currOpen}
+            onOpen={setCurrOpen}
+            title={"Test 1"}
+            num={22}
+            key={"test 1"}>
+            {"Lorum ..."}
+        </AccordionItem>
     </div>
-  );
+    );
+}
+
+function AccordionItem({num, title, currOpen, onOpen, children}) {
+    const isOpen = num === currOpen;
+
+    function handleToggle() {
+        onOpen(isOpen ? null : num);
+    }
+
+    return (<div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+        <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+        <p className="text">{title}</p>
+        <p className="icon">{isOpen ? "-" : "+"}</p>
+        {isOpen && <div className="content-box ">{children}</div>}
+    </div>);
 }
